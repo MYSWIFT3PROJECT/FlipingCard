59a8f7cb475ff88031fa854a0887d8ad1d616b03
NemSothea <nemsothea13@gmail.com>
2017-05-24 08:29:51 +0700

    Initial Commit

---
+++ FlibCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlibCard
+//
+//  Created by Nem Sothea on 5/24/17.
+//  Copyright © 2017 Nem Sothea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
